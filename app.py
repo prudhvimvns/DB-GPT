f2c7c2981d3703b8bf7577262c6775cc47281e98
M.V.N.S.Prudhvi <34057003+prudhvimvns@users.noreply.github.com>
2023-10-28 15:45:45 -0700

    update of removal csv

--- app.py
+++ app.py
@@ -183,7 +183,9 @@
 import psycopg2
 import tabulate
 import streamlit as st
-from langchain.agents import create_sql_agent,create_csv_agent
+# from langchain.agents import create_sql_agent,create_csv_agent
+from langchain.agents import create_sql_agent
+
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.agents.agent_types import AgentType
 from langchain.utilities import SQLDatabase
